57b7d3afa849ee34516ac5a98dbe7038c80713d5
Kenneth Combee <65651178+kennzenn@users.noreply.github.com>
2023-01-17 18:25:23 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 console.log("hi");
 
-const aantal=document.querySelector("main form div input");
+const aantal=document.querySelector("main form div output");
 
 const plus=document.querySelector("main form div button:nth-of-type(1)");
 
@@ -13,20 +13,20 @@ min.addEventListener("click",min1);
 
 
 function plus1(){
-    let currentAmount = parseInt(aantal.value);
+    let currentAmount = parseInt(aantal.innerHTML);
     let newAmount = currentAmount + 1;
-    aantal.value= newAmount;
+    aantal.innerHTML = newAmount;
 
     // en de min button enablen
     min.disabled=false;
 }
 
 function min1(){
-    let currentAmount = parseInt(aantal.value);
+    let currentAmount = parseInt(aantal.innerHTML);
 
     if(currentAmount > 0){
         let newAmount = currentAmount - 1;
-        aantal.value= newAmount;
+        aantal.innerHTML = newAmount;
 
         // als nieuwe aantal nul is
         if(newAmount == 0){
@@ -44,8 +44,8 @@ addToCart.addEventListener("click", updateCart);
 
 function updateCart(){
     let currentAmount=parseInt(cart.innerHTML);
-    let extraAmount=parseInt(aantal.value);
+    let extraAmount=parseInt(aantal.innerHTML);
     let newAmount=currentAmount+extraAmount;
     cart.innerHTML=newAmount;
-    aantal.value=0;
+    aantal.innerHTML = 0;
 }
